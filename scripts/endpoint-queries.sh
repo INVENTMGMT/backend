a8135067f5e933fb6382b2acffb65af47e0a8d14
Rob Scott <rdavidscott1@gmail.com>
2021-02-09 15:34:00 -0800

    endpoint trigger script

---
+++ scripts/endpoint-queries.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+curl \
+-X POST -H "Content-Type: application/json" \
+--data '{ "query": "{ addItem(name: \"succulent\", price:4, quantity:2){ name price quantity} }" }' \
+http://localhost:3000/dev/graphql
+
+echo "------------------------------------------------------------------"
+
+curl \
+-X POST -H "Content-Type: application/json" \
+--data '{ "query": "{ addItem(name: \"flowerpot\", price:15, quantity:1){ name price quantity} }" }' \
+http://localhost:3000/dev/graphql
+
+echo "------------------------------------------------------------------"
+
+curl \
+-X POST -H "Content-Type: application/json" \
+--data '{ "query": "{ getAllItems{ name price quantity } }" }' \
+http://localhost:3000/dev/graphql
