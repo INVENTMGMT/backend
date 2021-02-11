b9122e37decd26d0abb9f45f8e13ad0bf772f6d8
Rob Scott <rdavidscott1@gmail.com>
2021-02-11 12:17:00 -0800

    additional testing for getByName

--- scripts/endpoint-queries.sh
+++ scripts/endpoint-queries.sh
@@ -9,6 +9,12 @@ echo "------------------------------------------------------------------"
 
 curl \
 -X POST -H "Content-Type: application/json" \
+--data '{ "query": "{ addItem(name: \"succulent pot\", price:10, quantity:3){ name price quantity} }" }' \
+http://localhost:3000/dev/graphql
+
+echo "------------------------------------------------------------------"
+curl \
+-X POST -H "Content-Type: application/json" \
 --data '{ "query": "{ addItem(name: \"flowerpot\", price:15, quantity:1){ name price quantity} }" }' \
 http://localhost:3000/dev/graphql
 
@@ -17,4 +23,11 @@ echo "------------------------------------------------------------------"
 curl \
 -X POST -H "Content-Type: application/json" \
 --data '{ "query": "{ getAllItems{ name price quantity } }" }' \
+http://localhost:3000/dev/graphql
+
+echo "------------------------------------------------------------------"
+
+curl \
+-X POST -H "Content-Type: application/json" \
+--data '{ "query": "{ getByName(name: \"succulent\"){ name price quantity } }" }' \
 http://localhost:3000/dev/graphql
